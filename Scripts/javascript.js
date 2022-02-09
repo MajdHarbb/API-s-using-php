7dbca9c4a7e4501eb7d5f2bd6a28fcc31e4ab6c3
MajdHarbb <majdwalidharb@gmail.com>
2022-02-09 03:32:43 +0200

    view

--- Scripts/javascript.js
+++ Scripts/javascript.js
@@ -1,13 +1,16 @@
 $("document").ready(function () {
-  var btn = document.getElementById("calculate");
 
-  btn.addEventListener("click", myFunction);
+    $( "#calculate" ).click(myFunction);
+
 });
 
-function myFunction() {
-  var x = document.getElementById("number-1").value;
-  var y = document.getElementById("number-2").value;
+function calculateResult() {
+  
+  let x = $("#number-1").val();
+  let y = $("#number-2").val();
+  var calcResult = $("#calculate-result");
   $.getJSON(`my_api.php/?x=${x}&y=${y}`, function (json) {
     console.log(json.result);
+    calcResult.text(json.result);
   });
 }
